7364037dd48be6741612d535e9469a467f90438f
Amir_Razer <amirmahmoed003@gmail.com>
2023-04-09 11:20:07 +0700

    api login

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\API;
 
-use app\Helpers\ResponseFormatter;
+use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
+use app\Helpers\ResponseFormatter;
 use Laravel\Fortify\Rules\Password;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -40,6 +42,39 @@ class UserController extends Controller
                 'user' => $user
             ], 'User Registered');
         } catch (\Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error
+            ], 'Authentication Failed', 500);
+        }
+    }
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => ['email', 'required'],
+                'password' => ['required'],
+            ]);
+
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt($credentials)) {
+                return ResponseFormatter::error([
+                    'message' => 'Unauthorized'
+                ], 'Authentication Failed', 500);
+            }
+
+            $user = User::where('email', $request->email)->first();
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Invalid Credentials');
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 'Authenticated');
+        } catch (Exception $error) {
             return ResponseFormatter::error([
                 'message' => 'Something went wrong',
                 'error' => $error
